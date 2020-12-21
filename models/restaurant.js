a9a5ba60452952948c9b07e74aa8fa2772421355
bcbell <coleman.britt@gmail.com>
2020-12-21 01:36:26 -0500

    Updated restaurant form and schema  with list of states and territories

--- models/restaurant.js
+++ models/restaurant.js
@@ -10,7 +10,7 @@ const restSchema = new Schema(
         addressTwo: String,
         locality: String,
         city: String,
-        state: {type: String, enum:['al','ak','az','ar','ca',]},
+        state: {type: String, enum:['al','ak','az','ar','ca','co','ct','de','dc','fl','ga', 'hi','id','il','in','ia','ks','ky','la','me', 'md','ma','mi','mn','ms','mo','mt','ne', 'nv','nh','nj','nm','ny','nc','oh','ok','or','pa','ri','sc','sd','tn','tx','ut','vt','va','wa', 'wi','wy','as','gu','pr','vi']},
         zipcode: Number,
         speciality: String,
         features: {
