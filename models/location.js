f220047075fcd337ad6e85d02ed409a433fa55c6
bcbell <coleman.britt@gmail.com>
2020-12-21 03:17:41 -0500

    Updated: Added attraction type to location form

--- models/location.js
+++ models/location.js
@@ -19,7 +19,23 @@ const locationSchema = new Schema(
         placecity: String,
         state: {type: String, enum:['al','ak','az','ar','ca','co','ct','de','dc','fl','ga', 'hi','id','il','in','ia','ks','ky','la','me', 'md','ma','mi','mn','ms','mo','mt','ne', 'nv','nh','nj','nm','ny','nc','oh','ok','or','pa','ri','sc','sd','tn','tx','ut','vt','va','wa', 'wi','wy','as','gu','pr','vi']},
         placezipcode: Number,
-        locationType: String,
+        locationType: {
+          type: 'object',
+          properties: {
+            param: {
+              oneOf:[
+                {
+                enum:['National Park','Beaches', 'Caves', 'Mountains', 'Hills', 'Waterfalls', 'Islands', 'Forest','Entertainment Park', 'Wildlife', 'Aquarium','Botanical Garden', 'Museum', 'Art Gallery', 'Historical or Heritage','Spectating Sports Atrraction', 'Participating Sports Attraction', 'Stadium Tour', 'Market', 'Festivals', 'Parade', 'Exhibitions', 'Entertainment Venue']
+              },{
+                type:'array',
+                items: {type : 'integer'}
+              }              
+            ]
+            }
+          },
+          additionalProperties : false,
+          required: ['param'],
+        },
         age : {
           type: 'object',
           properties: {
